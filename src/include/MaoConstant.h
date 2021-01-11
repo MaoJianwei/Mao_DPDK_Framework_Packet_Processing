0fd85fad5ed9170b93fd1ce56b12afd6b012ec9e
Jianwei Mao <maojianwei2012@126.com>
2021-01-11 05:54:54 -0800

    Update architecture
    1. change to static link
    2. add lcore_runtime
    3. add lcore_config injection method for debug
    4. add lcore_config checking method
    5. initial init_port
    
    Signed-off-by: Jianwei Mao <maojianwei2012@126.com>

--- src/include/MaoConstant.h
+++ src/include/MaoConstant.h
@@ -2,11 +2,24 @@
 
 #define RTE_LOGTYPE_Mao RTE_LOGTYPE_USER1
 
+// capabilities of framework
+#define Mao_MAX_SOCKET 2 // Can you help us to test the case of more than 2 sockets?
 #define Mao_MAX_LCORE RTE_MAX_LCORE // RTE_MAX_LCORE 128
 #define Mao_MAX_ETHPORTS RTE_MAX_ETHPORTS // RTE_MAX_ETHPORTS 32
+
+// capabilities of applications
+#define Mao_MAX_PORTS_PER_LCORE 4
+
 
 
 
 // solid configuration
-#define Mao_RX_QUEUE_NUM_PER_PORT 1
-#define Mao_TX_QUEUE_NUM_PER_PORT 1
+#define Mao_RX_QUEUE_PER_PORT 1
+#define Mao_TX_QUEUE_PER_PORT 1
+
+#define Mao_RX_DESC_PER_PORT 1024
+#define Mao_TX_DESC_PER_PORT 1024
+
+
+// general configuration
+#define Mao_MAX_PACKET_BURST 32
--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <signal.h>
 #include <rte_eal.h>
 #include <rte_ethdev.h>
+#include <rte_malloc.h>
 
 
 #include "include/MaoConstant.h"
@@ -11,17 +12,36 @@
 
 
 
-struct port_runtime {
-    unsigned short port_id;
-    unsigned short lcore_id;
-    unsigned xxx tx_buffer;
-    unsigned xxx rx_buffer;
-    unsigned char assigned; // avoid to read unused port
+//struct port_runtime {
+//    unsigned short port_id;
+//    unsigned short lcore_id;
+////    unsigned xxx tx_buffer;
+////    unsigned xxx rx_buffer;
+//    unsigned char assigned; // avoid to read unused port
+//};
+
+struct lcore_runtime {
+    unsigned int lcore_id;
+
+    unsigned int nb_rx_port;
+    unsigned int rx_port_ids[Mao_MAX_PORTS_PER_LCORE];
+
+    unsigned int nb_tx_port;
+    unsigned int tx_port_ids[Mao_MAX_PORTS_PER_LCORE];
+    struct rte_eth_dev_tx_buffer * tx_buffers[Mao_MAX_PORTS_PER_LCORE];
 };
 
 
+// all runtime variables
+
+struct lcore_runtime lcore_runtime_config[Mao_MAX_LCORE];
+struct rte_mempool* rx_pktmbuf_pool[Mao_MAX_SOCKET];
+
 unsigned char need_shutdown = 0;
-struct port_runtime port_runtimes[Mao_MAX_ETHPORTS];
+//struct port_runtime port_runtimes[Mao_MAX_ETHPORTS];
+
+
+
 
 static void sys_signal_processor(int signal) {
     switch(signal) {
@@ -42,9 +62,69 @@ void init_power_subsystem() {
     ;
 }
 
-void check_lcore_config() {
-    // todo
-    ;
+void debug_inject_lcore_config() {
+    lcore_runtime_config[0].lcore_id = 0;
+    lcore_runtime_config[0].nb_rx_port++;
+    lcore_runtime_config[0].rx_port_ids[0] = 0;
+    lcore_runtime_config[0].nb_tx_port++;
+    lcore_runtime_config[0].tx_port_ids[0] = 0;
+    lcore_runtime_config[0].tx_buffers[0] = rte_zmalloc_socket("lcore0_tx_buffer",
+        RTE_ETH_TX_BUFFER_SIZE(Mao_MAX_PACKET_BURST), 0,rte_eth_dev_socket_id(0));
+
+    lcore_runtime_config[1].lcore_id = 1;
+    lcore_runtime_config[1].nb_rx_port++;
+    lcore_runtime_config[1].rx_port_ids[0] = 1;
+    lcore_runtime_config[1].nb_tx_port++;
+    lcore_runtime_config[1].tx_port_ids[0] = 1;
+    lcore_runtime_config[1].tx_buffers[0] = rte_zmalloc_socket("lcore1_tx_buffer",
+        RTE_ETH_TX_BUFFER_SIZE(Mao_MAX_PACKET_BURST), 0,rte_eth_dev_socket_id(1));
+};
+
+unsigned char check_lcore_config() {
+    //FIXME: may be enhanced.
+
+    unsigned int i, j;
+    for (i = 0; i < Mao_MAX_SOCKET; i++) {
+        if (lcore_runtime_config[i].lcore_id >= Mao_MAX_LCORE ||
+            lcore_runtime_config[i].nb_rx_port > Mao_MAX_PORTS_PER_LCORE ||
+            lcore_runtime_config[i].nb_tx_port > Mao_MAX_PORTS_PER_LCORE)
+            return -1;
+
+        for (j = 0; j < lcore_runtime_config[i].nb_rx_port; j++) {
+            if (lcore_runtime_config[i].rx_port_ids[j] >= Mao_MAX_ETHPORTS)
+                return -1;
+        }
+        for (j = 0; j < lcore_runtime_config[i].nb_tx_port; j++) {
+            if (lcore_runtime_config[i].tx_port_ids[j] >= Mao_MAX_ETHPORTS)
+                return -1;
+        }
+    }
+    return 0;
+}
+
+void init_lcore() {
+
+}
+
+void init_pktmbuf_mem_pool() {
+
+/*
+ * This expression is used to calculate the number of mbufs needed depending on
+ * user input, taking into account memory for rx and tx hardware rings, cache
+ * per lcore and mtable per port per lcore. RTE_MAX is used to ensure that
+ * NB_MBUF never goes below a minimum value of 8192.
+ */
+//    uint16_t mempool_size = RTE_MAX	( \
+//	(nb_ports*nb_rx_queue*nb_rxd + \
+//	nb_ports*nb_lcores*MAX_PKT_BURST + \
+//	nb_ports*n_tx_queue*nb_txd + \
+//	nb_lcores*MEMPOOL_CACHE_SIZE), \
+//	(unsigned)8192)
+
+    uint16_t mempoll_size = 8192;
+
+    //fixme: todo
+    unsigned int nb_socket = RTE_MIN(rte_socket_count(), Mao_MAX_SOCKET);
 }
 
 void init_port() {
@@ -57,10 +137,17 @@ void init_port() {
                     .offloads = DEV_TX_OFFLOAD_IPV4_CKSUM | DEV_TX_OFFLOAD_TCP_CKSUM | DEV_TX_OFFLOAD_UDP_CKSUM,
             }
     };
-    
+
+    uint16_t nb_ports = rte_eth_dev_count_avail();
+    RTE_LOG(INFO, Mao, "Port count %d\n", nb_ports);
+
+
+    uint16_t nb_rx_desc = Mao_RX_DESC_PER_PORT;
+    uint16_t nb_tx_desc = Mao_TX_DESC_PER_PORT;
+    uint64_t port_id;
     RTE_ETH_FOREACH_DEV(port_id) {
-        rte_eth_dev_configure(port_id, Mao_RX_QUEUE_NUM_PER_PORT, Mao_TX_QUEUE_NUM_PER_PORT, &rte_port_config);
-        rte_eth_dev_adjust_nb_rx_tx_desc(port_id, )
+        rte_eth_dev_configure(port_id, Mao_RX_QUEUE_PER_PORT, Mao_TX_QUEUE_PER_PORT, &rte_port_config);
+        rte_eth_dev_adjust_nb_rx_tx_desc(port_id, &nb_rx_desc, &nb_tx_desc);
     }
 }
 
@@ -74,25 +161,32 @@ int main (int argc, char ** argv) {
     printf("Mao: Init EAL...\n");
     ret = rte_eal_init(argc, argv);
     if (ret < 0) {
-        RTE_LOG(ERR, Mao, "Init EAL: Fail, invalid EAL parameters\n");
+        RTE_LOG(ERR, Mao, "Fail, invalid EAL parameters\n");
         rte_exit(EXIT_FAILURE, "Invalid EAL parameters\n");
     }
 
     init_timer_subsystem();
     init_power_subsystem();
 
-    check_lcore_config();
-    init_lcore();
+    debug_inject_lcore_config();
+    ret = check_lcore_config();
+    if (ret < 0) {
+        RTE_LOG(ERR, Mao, "Fail, invalid lcore config.\n");
+        rte_exit(EXIT_FAILURE, "Invalid lcore config");
+    }
 
-    check_port_config();
+    init_lcore();
+    init_pktmbuf_mem_pool();
+
+//    check_port_config();
     init_port();
 
-    launch_port();
-
-    wait_for_complete();
-
-
-    clean(); // wait for detail
+//    launch_port();
+//
+//    wait_for_complete();
+//
+//
+//    clean(); // wait for detail
 
     goto cleanup;
 
